7fd7a39bad6a214b742003be4a32c4c0c3f83351
Dandresfs <dandresfsoto@gmail.com>
2018-05-27 21:59:34 -0500

    Primera distibución

---
+++ README.md
@@ -0,0 +1,7 @@
+=====
+django-divipola-colombia
+=====
+
+django-divipola-colombia es una app simple para implementar en Django la división político administrativa de colombia Divipola
+
+La documentación detallada se encuentra en el directorio "docs".
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-divipola-colombia',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='Una app simple para implementar en Django la división político administrativa de colombia Divipola',
+    long_description=README,
+    url='https://github.com/Dandresfsoto/django_divipola/tree/master/django_divipola_colombia',
+    author='Diego Andres Fonseca Soto',
+    author_email='dandresfsoto@gmail.com',
+    install_requires=[
+        'Django>=2.0'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0.5',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
